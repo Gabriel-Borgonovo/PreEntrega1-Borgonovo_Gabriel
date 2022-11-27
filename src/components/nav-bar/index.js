1e226632084bc83b8095e25b0f3916adfe211b2d
Gabriel Borgonovo <gabrielalejandroborgonovo@gmail.com>
2022-11-27 15:17:40 -0300

    Creación proyecto react, primer componente y importando bootstrap

---
+++ src/components/nav-bar/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./index.css"
+
+export default function NavBar(){
+    return(
+        <header>
+            <h1>Voy a ser una barra de navegación</h1>
+        </header>
+    );
+}
